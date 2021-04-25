6cfd1b86e1d21d162df3b9390971d6702ea042b7
Armin Kirchner <post.armin@gmail.com>
2021-04-25 23:09:47 +0900

    parse points from well known text

--- test/test.rb
+++ test/test.rb
@@ -1,13 +1,76 @@
 require 'minitest/autorun'
 require 'byebug'
 
+class Parser
+  POINT = /\APOINT\ \((?<points>.+)\)\z/
+
+  def self.from_wkt(well_known_text)
+    new(well_known_text)
+  end
+
+  def initialize(wkt)
+    @wkt = wkt
+    @points = wkt.match(POINT)[:points].split(',').map do |wkt_point|
+      x, y, z = wkt_point.split(" ")
+      Point.new(x: x, y: y, z: z)
+    end
+  end
+
+  def to_wkt
+    @wkt
+  end
+
+  def points
+    @points
+  end
+end
+
+class ParserTest < Minitest::Test
+  def test_point_parsing
+    parser = Parser.from_wkt('POINT (30 10)')
+
+    assert_equal parser.to_wkt, 'POINT (30 10)'
+    assert_equal parser.points, [Point.new(x: 30, y: 10, z: nil)]
+  end
+
+  # def test_point_z_parsing
+  #   parser = Parser.from_wkt('POINT Z (30 10 5)')
+
+  #   assert_equal parser.to_wkt, 'POINT Z (30 10 5)'
+  #   assert_equal parser.points, [Point.new(x: 30, y: 10, z: 5)]
+  # end
+
+  # def test_line_string_parsing
+  #   parser = Parser.from_wkt('LINESTRING (30 10, 10 30, 40 40)')
+
+  #   assert_equal parser.to_wkt, 'LINESTRING (30 10, 10 30, 40 40)'
+  #   assert_equal parser.points, [
+  #     Point.new(x: 30, y: 10, z: nil),
+  #     Point.new(x: 10, y: 30, z: nil),
+  #     Point.new(x: 40, y: 40, z: nil)
+  #   ]
+  # end
+
+  # def test_line_string_z_parsing
+  #   parser = Parser.from_wkt('LINESTRING Z (30 10 40, 10 30 20, 40 40 10)')
+
+  #   assert_equal parser.to_wkt, 'LINESTRING Z (30 10 40, 10 30 20, 40 40 10)'
+  #   assert_equal parser.points, [
+  #     Point.new(x: 30, y: 10, z: 40),
+  #     Point.new(x: 10, y: 30, z: 20),
+  #     Point.new(x: 40, y: 40, z: 10)
+  #   ]
+  # end
+end
+
 class Point
   attr_reader :x, :y, :z
 
+
   def initialize(x:, y:, z:)
-    @x = x
-    @y = y
-    @z = z
+    @x = x.is_a?(String) ? x.to_i : x
+    @y = y.is_a?(String) ? y.to_i : y
+    @z = z.is_a?(String) ? z.to_i : z
   end
 
   def ==(other)
